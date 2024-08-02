bebdb69fe6d873962aaed5a45cf2444e5d5c944c
FabioSpoto2023 <FabioSpoto2023@gmail.com>
2024-08-02 12:16:24 +0200

    firebase project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 
 @NgModule({
@@ -18,7 +20,9 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   ],
   providers: [
     provideClientHydration(),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp({"projectId":"simple-crm-8ff23","appId":"1:299734421815:web:a67c36cf319e424a1acab2","storageBucket":"simple-crm-8ff23.appspot.com","apiKey":"AIzaSyD96NMvSccjY7y2hyHlN5N795JMHR9RZDQ","authDomain":"simple-crm-8ff23.firebaseapp.com","messagingSenderId":"299734421815"})),
+    provideFirestore(() => getFirestore())
   ],
   bootstrap: [AppComponent]
 })
