83c7bc62ad8878f20ff7c8abc938fcb055e5eeaf
FabioSpoto2023 <FabioSpoto2023@gmail.com>
2024-08-02 13:34:20 +0200

    project bug fix

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -4,11 +4,9 @@ import { provideNativeDateAdapter } from '@angular/material/core';
 import { User } from '../../models/user.class';
 import { MaterialModule } from '../material/material.module';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
+
 import { addDoc, collection, CollectionReference, doc, Firestore, setDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-=======
->>>>>>> 9b49298 (install firebase)
 
 
 @Component({
@@ -25,16 +23,9 @@ export class DialogAddUserComponent {
 
   firestore: Firestore = inject(Firestore);
 
-<<<<<<< HEAD
-  user = new User();
-  birthDate = new Date();
-=======
-  dialogRef = inject(MatDialogRef<DialogAddUserComponent>);
->>>>>>> 9b49298 (install firebase)
 
   user = new User();
   birthDate = new Date();
-  
 
   closeDialog() {
     this.dialogRef.close();
@@ -43,12 +34,9 @@ export class DialogAddUserComponent {
   saveUser() {
     this.user.birthDate = this.birthDate.getTime();
     console.log('User:', this.user);
-<<<<<<< HEAD
 
     addDoc(collection(this.firestore, 'users'), this.user.toJSON());
 
-=======
->>>>>>> 9b49298 (install firebase)
     this.closeDialog();
   }
 
