bdd4de300ae32b6194a9b28e4b230e813a277c5c
FabioSpoto2023 <FabioSpoto2023@gmail.com>
2024-08-02 13:08:15 +0200

    add user to database

---
+++ src/app/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    "projectId": "simple-crm-8ff23",
+    "appId": "1:299734421815:web:a67c36cf319e424a1acab2",
+    "storageBucket": "simple-crm-8ff23.appspot.com",
+    "apiKey": "AIzaSyD96NMvSccjY7y2hyHlN5N795JMHR9RZDQ",
+    "authDomain": "simple-crm-8ff23.firebaseapp.com",
+    "messagingSenderId": "299734421815"
+}
