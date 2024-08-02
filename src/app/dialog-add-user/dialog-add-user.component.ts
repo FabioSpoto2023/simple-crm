9b492988eae13d3882c49617d47e03a49b53e6dc
FabioSpoto2023 <FabioSpoto2023@gmail.com>
2024-08-02 12:14:51 +0200

    install firebase

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -3,27 +3,32 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { User } from '../../models/user.class';
 import { MaterialModule } from '../material/material.module';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [MaterialModule, FormsModule],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss',
   providers: [provideNativeDateAdapter()]
 })
 export class DialogAddUserComponent {
 
-  user = new User();
   dialogRef = inject(MatDialogRef<DialogAddUserComponent>);
+
+  user = new User();
+  birthDate = new Date();
 
   closeDialog() {
     this.dialogRef.close();
   }
 
   saveUser() {
+    this.user.birthDate = this.birthDate.getTime();
     console.log('User:', this.user);
+    this.closeDialog();
   }
 
 }
