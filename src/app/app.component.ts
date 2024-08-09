dc6456b67fa3e35b49e7749230bd90c2df0bf92b
FabioSpoto2023 <FabioSpoto2023@gmail.com>
2024-08-09 11:40:12 +0200

    changed nav selection function and added router url to find the right selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from 'express';
+import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'simple-crm';
 
-  ngOnInit(): void {
-    this.sideNavSelect('dashboard');
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.setNavSelection(event.url);
+      }
+    })
   }
 
-  sideNavSelect(selection: string) {
+  setNavSelection(url: string) {
     if (typeof document !== 'undefined') {
       let itemDashboard = document.getElementById('sidenav-dashboard');
       let itemClients = document.getElementById('sidenav-clients');
       let itemSettings = document.getElementById('sidenav-settings');
       let itemLogout = document.getElementById('sidenav-logout');
 
-      if (selection == 'dashboard') {
+      if (url == '/') {
         itemDashboard?.classList.add('select-dashboard');
         itemClients?.classList.remove('select-clients');
         itemSettings?.classList.remove('select-settings');
         itemLogout?.classList.remove('select-logout');
-      } else if (selection == 'clients') {
+      } else if (url == '/clients') {
         itemClients?.classList.add('select-clients');
         itemDashboard?.classList.remove('select-dashboard');
         itemSettings?.classList.remove('select-settings');
         itemLogout?.classList.remove('select-logout');
-      } else if (selection == 'settings') {
+      } else if (url == '/settings') {
         itemSettings?.classList.add('select-settings');
         itemDashboard?.classList.remove('select-dashboard');
         itemClients?.classList.remove('select-clients');
         itemLogout?.classList.remove('select-logout');
-      } else if (selection == 'logout') {
+      } else if (url == '/logout') {
         itemLogout?.classList.add('select-logout');
         itemDashboard?.classList.remove('select-dashboard');
         itemClients?.classList.remove('select-clients');
         itemSettings?.classList.remove('select-settings');
-      } else {
-        itemDashboard?.classList.remove('select-dashboard');
-        itemClients?.classList.remove('select-clients');
-        itemSettings?.classList.remove('select-settings');
-        itemLogout?.classList.remove('select-logout');
       }
     }
   }
